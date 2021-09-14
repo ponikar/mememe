e103d88a87a18ee1056e0100b763c9f169fb29a0
ponikar <drsofficial3k@gmail.com>
2021-09-14 12:38:42 +0530

    working with whatsapp ui, adding chat design

--- src/App.tsx
+++ src/App.tsx
@@ -17,11 +17,11 @@ const App: FC = () => {
             <div className="flex items-center">
               <img src="./src/svgs/arrow-left.svg" />
               <img
-                src="./src/svgs/avatar.svg"
+                src="./src/svgs/pp.jpeg"
                 className="rounded-full ml-1 h-9 w-9 object-contain"
               />
               <div className="ml-2 text-android-whatsapp-highlight">
-                <h2> Ted Mosby </h2>
+                <h2 className="font-semibold"> Umesh Ojha </h2>
                 <h3 className="text-xs"> Online </h3>
               </div>
             </div>
@@ -41,15 +41,68 @@ const App: FC = () => {
             </div>
           </nav>
 
-          <section className="flex w-full justify-center">
+          <section className="flex my-2 w-full justify-center">
             <div className="px-2 shadow-sm text-android-whatsapp-label-text p-1 rounded-md text-xs bg-android-whatsapp-label">
               TODAY
             </div>
           </section>
-          <section className="flex-grow px-2">
-            <section className="flex w-full justify-end">
-              <div className="p-2 shadow-sm text-sm text-black rounded-xl bg-android-whatsapp-chat">
-                Hey dude!
+          <section className="flex-grow px-5">
+            <section className="flex my-2 w-full">
+              <div
+                style={{
+                  width: "auto",
+                  maxWidth: "80%",
+                  paddingRight: "10px",
+                  paddingBottom: "10px",
+                }}
+                className="break-all items-start  flex relative shadow-sm text-sm text-black rounded-md bg-white"
+              >
+                <p className="p-2">
+                  {" "}
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Temporibus optio dolorum, enim quia voluptatem voluptas eum
+                  commodi porro, inventore voluptatum tempora quibusdam
+                  similique facere laboriosam harum. Libero hic voluptas cum.{" "}
+                </p>
+                <div className="flex bottom-0  right-1 px-1 absolute">
+                  <div
+                    style={{ fontSize: "11px" }}
+                    className="ml-2 text-android-whatsapp-label-text"
+                  >
+                    11:30 AM
+                  </div>
+                  {/* <img src="./src/svgs/blue-tick.svg" className="w-5 ml-1" /> */}
+                </div>
+                <img
+                  src="./src/svgs/whatsapp-msg-receive.svg"
+                  className="absolute -left-2 top-0"
+                />
+              </div>
+            </section>
+            <section className="flex my-2 w-full justify-end">
+              <div
+                style={{
+                  width: "auto",
+                  maxWidth: "80%",
+                  paddingRight: "70px",
+                  paddingBottom: "10px",
+                }}
+                className="break-all items-start  flex relative shadow-sm text-sm text-black rounded-md bg-android-whatsapp-chat"
+              >
+                <p className="p-2"> Achha! </p>
+                <div className="flex bottom-0  right-0 px-1 absolute">
+                  <div
+                    style={{ fontSize: "11px" }}
+                    className="ml-2 text-android-whatsapp-label-text"
+                  >
+                    11:35 AM
+                  </div>
+                  <img src="./src/svgs/blue-tick.svg" className="w-5 ml-1" />
+                </div>
+                <img
+                  src="./src/svgs/whatsapp-msg-send.svg"
+                  className="absolute -right-2 top-0"
+                />
               </div>
             </section>
           </section>
@@ -72,7 +125,7 @@ const App: FC = () => {
                 alt=""
               />
             </section>
-            <button className="bg-android-whatsapp-primary w-12 flex items-center justify-center ml-1 rounded-full shadow-sm p-1">
+            <button className="bg-android-whatsapp-primary w-11 flex items-center justify-center ml-1 rounded-full shadow-sm p-1">
               <img className="w-5" src="./src/svgs/mic.svg" />
             </button>
           </section>
